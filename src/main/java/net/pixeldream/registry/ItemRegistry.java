24b7e34f60bc8e5ce3f58248d88df1d3f6b10440
kyber-6 <100732009+kyber-6@users.noreply.github.com>
2023-07-05 18:24:56 +0200

    Changed maxCount

--- src/main/java/net/pixeldream/registry/ItemRegistry.java
+++ src/main/java/net/pixeldream/registry/ItemRegistry.java
@@ -8,7 +8,7 @@ import net.minecraft.util.Identifier;
 import net.pixeldream.Valor;
 
 public class ItemRegistry {
-    public static final Item FIRST_DUNGEON_KEY = registerItem("first_dungeon_key", new Item(new FabricItemSettings()));
+    public static final Item FIRST_DUNGEON_KEY = registerItem("first_dungeon_key", new Item(new FabricItemSettings().maxCount(1)));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, new Identifier(Valor.MOD_ID, name), item);
