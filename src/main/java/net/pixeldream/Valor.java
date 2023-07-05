b6cee6569832d1135606667587de0a3b2ba3eae1
kyber-6 <100732009+kyber-6@users.noreply.github.com>
2023-07-05 17:51:36 +0200

    Added MOD_ID field

--- src/main/java/net/pixeldream/Valor.java
+++ src/main/java/net/pixeldream/Valor.java
@@ -1,15 +1,18 @@
 package net.pixeldream;
 
 import net.fabricmc.api.ModInitializer;
-
+import net.pixeldream.registry.ItemGroupRegistry;
+import net.pixeldream.registry.ItemRegistry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Valor implements ModInitializer {
-    public static final Logger LOGGER = LoggerFactory.getLogger("valor");
+    public static final String MOD_ID = "valor";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	@Override
-	public void onInitialize() {
-
-	}
+    @Override
+    public void onInitialize() {
+        ItemRegistry.init();
+        ItemGroupRegistry.init();
+    }
 }
