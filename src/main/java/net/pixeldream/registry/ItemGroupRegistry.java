272affd6b60a962cb72dbfc3e66e7204dbff95c4
kyber-6 <100732009+kyber-6@users.noreply.github.com>
2023-07-05 17:50:52 +0200

    Item group registry

---
+++ src/main/java/net/pixeldream/registry/ItemGroupRegistry.java
@@ -0,0 +1,23 @@
+package net.pixeldream.registry;
+
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+import net.pixeldream.Valor;
+
+public class ItemGroupRegistry {
+    public static final ItemGroup VALOR_GROUP = Registry.register(Registries.ITEM_GROUP, new Identifier(Valor.MOD_ID, "valor"), FabricItemGroup.builder().displayName(Text.translatable("itemgroup.valor")).icon(() -> new ItemStack(Items.RED_MUSHROOM)).entries((displayContext, entries) -> {
+        entries.add(ItemRegistry.FIRST_DUNGEON_KEY);
+    }).build());
+
+
+
+    public static void init() {
+        Valor.LOGGER.info("Registering item group for " + Valor.MOD_ID);
+    }
+}
