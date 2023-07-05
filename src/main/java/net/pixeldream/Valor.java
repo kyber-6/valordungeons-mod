c3217c203d691e493b02671123e36b8c7f0500d4
kyber-6 <100732009+kyber-6@users.noreply.github.com>
2023-07-05 17:09:36 +0200

    Initial commit

---
+++ src/main/java/net/pixeldream/Valor.java
@@ -0,0 +1,15 @@
+package net.pixeldream;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Valor implements ModInitializer {
+    public static final Logger LOGGER = LoggerFactory.getLogger("valor");
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
