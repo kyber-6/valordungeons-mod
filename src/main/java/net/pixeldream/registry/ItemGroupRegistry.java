2fd11cfa6ca94c72c0ae4a4b213f5cd7e5dd76c1
kyber-6 <100732009+kyber-6@users.noreply.github.com>
2023-07-05 18:24:24 +0200

    Change

--- src/main/java/net/pixeldream/registry/ItemGroupRegistry.java
+++ src/main/java/net/pixeldream/registry/ItemGroupRegistry.java
@@ -11,7 +11,7 @@ import net.minecraft.util.Identifier;
 import net.pixeldream.Valor;
 
 public class ItemGroupRegistry {
-    public static final ItemGroup VALOR_GROUP = Registry.register(Registries.ITEM_GROUP, new Identifier(Valor.MOD_ID, "valor"), FabricItemGroup.builder().displayName(Text.translatable("itemgroup.valor")).icon(() -> new ItemStack(Items.RED_MUSHROOM)).entries((displayContext, entries) -> {
+    public static final ItemGroup VALOR_GROUP = Registry.register(Registries.ITEM_GROUP, new Identifier(Valor.MOD_ID, Valor.MOD_ID), FabricItemGroup.builder().displayName(Text.translatable("itemgroup." + Valor.MOD_ID)).icon(() -> new ItemStack(Items.RED_MUSHROOM)).entries((displayContext, entries) -> {
         entries.add(ItemRegistry.FIRST_DUNGEON_KEY);
     }).build());
 
