44e61f8f32f48bc1160628d641c9f1cd3a220883
kyber-6 <100732009+kyber-6@users.noreply.github.com>
2023-10-18 13:40:14 +0200

    azurelib + cleanup

--- src/main/java/net/pixeldream/valor/Valor.java
+++ src/main/java/net/pixeldream/valor/Valor.java
@@ -1,12 +1,11 @@
 package net.pixeldream.valor;
 
+import mod.azure.azurelib.AzureLib;
 import net.fabricmc.api.ModInitializer;
-import net.pixeldream.valor.registry.EntityRegistry;
 import net.pixeldream.valor.registry.ItemGroupRegistry;
 import net.pixeldream.valor.registry.ItemRegistry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import software.bernie.geckolib.GeckoLib;
 
 public class Valor implements ModInitializer {
     public static final String MOD_ID = "valor";
@@ -14,9 +13,8 @@ public class Valor implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        GeckoLib.initialize();
+        AzureLib.initialize();
         ItemRegistry.initialize();
         ItemGroupRegistry.initialize();
-        EntityRegistry.initialize();
     }
 }
--- src/main/java/net/pixeldream/valor/ValorClient.java
+++ src/main/java/net/pixeldream/valor/ValorClient.java
@@ -2,12 +2,10 @@ package net.pixeldream.valor;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
-import net.pixeldream.valor.entity.client.SpiritFoxRenderer;
-import net.pixeldream.valor.registry.EntityRegistry;
 
 public class ValorClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        EntityRendererRegistry.register(EntityRegistry.SPIRIT_FOX, SpiritFoxRenderer::new);
+
     }
 }
